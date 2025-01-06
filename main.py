2ce45faa28caa115dfa8d1e9fa77bdaab9a08fbe
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-01-05 21:16:07 -0500

    added changing direction functionality using screen listener

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
+import time
 from turtle import Turtle, Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.bgcolor("black")
 screen.title("Snake Game")
-starting_position = [(0,0), (-20,0), (-40,0)]
-segments = []
-#Turtle.speed()
 
-for position in starting_position:
-    segment = Turtle("square")
-    segment.color("white")
-    segment.penup()
-    segment.goto(position)
-    segments.append(segment)
+
+#Turtle.speed()
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    for segment in segments:
-        segment.forward(20)
+    time.sleep(0.1)
+    #snake direction would be decided by the head or first segment,
+    # thus tracing by the remaining segments for easy follow movement
+    snake.move()
 
 
 screen.exitonclick()
